19cfdcdaad2212cf026746f7d7ca38760d824a7e
Theofahey <tfahey20@stuy.edu>
2021-09-29 23:47:51 -0400

    Updated HW#6

--- 06_py/jobs.py
+++ 06_py/jobs.py
@@ -1,31 +1,56 @@
-# Michelle Lo, Theodore Fahey, Rayat Roy
+# Lone Devos: Michelle Lo, Rayat Roy, Theodore Fahey
 # SoftDev
-#K<06> -- <Title/Topic/Summary... (Aim for concision, brevity, CLARITY. Write to your future self...)
-# Reads the csv file and then delimits it with a comma. Creates a Dictionary and then added keys for the job titles and
-# values for their respective chance. Then we created a random and then parced through the values of the dict. Adding their
-#Percentages a total value until it is greater than the random value. Then prints the key of that value.
-# <yyyy>-<mm>-<dd>
+# K<06> -- Reading CSV file + weighted probabilities
+# <2021>-<09>-<28>
+
+##SUMMARY
+##Our team first discussed how to parse through a csv file.
+##We also reviewed how to use and set up a dictionary.Then, we discussed how we can generate
+##a random occupation based on its probability.
 
 import csv
 import random
 
-with open('occupations.csv') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter= ",")
-    jobs = {}
-    i = 0;
-    for row in spamreader:
-        if (i == 0):
-            i += 1
-        else:
-            jobs[row[0]] = float(row[1]);
-        
-    #random selector
-r = random.random() 
-s = 0
-for key in jobs:
-    s+= jobs[key]/100
-    if(s > r):
-        print(key)
-        break
+#reading csv file
+def reader():
+    with open('occupations.csv') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter= ",")
+        jobs = {} #dictionary created
+        i = 0;
+        for row in spamreader:
+            if (i == 0):
+                i += 1 #skips heading
+            else:
+                jobs[row[0]] = float(row[1]);
+        return jobs
+            
+#random selector
+def randSelect(jobs):
+    r = random.random() #generates a random float from 0 to 1
+    s = 0
+    for key in jobs: #for every occupation
+        s+= jobs[key]/100 #add occupation's percentage to s
+        if(s > r): #if combined percentages is larger than random number,
+            #print(key) #return final occupation
+            #break
+            return key
 
-#dictionary
+#randChoices utilizes random.choices to randomly choose an element based on its weighted probability
+def randChoices(jobs):
+    jobList = list(jobs.keys()) #turns dict keys into a list
+    percentages = list(jobs.values()) #dict values into a list
+    fin = random.choices(jobList, weights = percentages, k = 1) #generates a list of 1 randomly chosen occupation
+    return fin[0]
+
+def main():#runs program
+    print(randSelect(reader()))
+    print(randChoices(reader()))
+
+if __name__ == '__main__':
+    main()
+    
+
+
+    
+
+
