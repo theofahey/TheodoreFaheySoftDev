837250ba0ad891dcc7ab01afc3e2a72ee0bcec29
Theofahey <tfahey20@stuy.edu>
2021-10-17 22:25:31 -0400

    edited notes

--- 14_form/app.py
+++ 14_form/app.py
@@ -1,6 +1,7 @@
-# Clyde 'Thluffy' Sinclair
+# Team Snaps (Emma Buller, Theo Fahey, Angela Zhang)
 # SoftDev
-# Oct 2021 
+# K14 -- Form and Function (Flask and Forms)
+# 2021-10-14
 
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
@@ -26,36 +27,36 @@ PROTIP: Insert your own in-line comments wherever they will help your future sel
 def disp_loginpage():
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
-    print(app)
+    print(app) #prints app
     print("***DIAG: request obj ***")
-    print(request)
+    print(request) #prints link and methods
     print("***DIAG: request.args ***")
-    print(request.args)
-    #print("***DIAG: request.args['username']  ***")
+    print(request.args) #prints empty dictionary since there have been no arguments inputted
+    #print("***DIAG: request.args['username']  ***") <- only works after submitting form
     #print(request.args['username'])
     print("***DIAG: request.headers ***")
-    print(request.headers)
-    return render_template( 'login.html' )
+    print(request.headers)  #prints various information
+    return render_template( 'login.html' ) #webpage is just the login.html file in templates
 
 
 @app.route("/auth") # , methods=['GET', 'POST'])
 def authenticate():
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
-    print(app)
+    print(app) #same thing as disp_loginpage()
     print("***DIAG: request obj ***")
-    print(request)
+    print(request) #link of page (with the inputs from the form)
     print("***DIAG: request.args ***")
-    print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
+    print(request.args) #not empty anymore
+    print("***DIAG: request.args['username']  ***")
+    print(request.args['username']) #prints the username that you inputted
     print("***DIAG: request.headers ***")
-    print(request.headers)
-    #return "Waaaa hooo HAAAH"  #response to a form submission
-    return render_template('response.html', username = request.args['username'], method = request.args['sub1'])
+    print(request.headers) #same as disp_loginpage()
+    m = request.method #either get or post
+    return render_template('response.html', username = request.args['username'], method = m)
 
-    
+
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
-    app.debug = True 
+    app.debug = True
     app.run()
